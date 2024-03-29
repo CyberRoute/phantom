b7c24590987da242ae04878604054fb4aac56711
CyberRoute <alessandro.bresciani2016@gmail.com>
2024-03-29 21:09:59 +0100

    caching data file

--- core/arp_scanner.py
+++ core/arp_scanner.py
@@ -10,6 +10,7 @@ import socket
 import netifaces
 import core.networking as net
 import core.sniffer as sniffer
+import os
 
 
 class MacVendorLookup:
@@ -18,16 +19,49 @@ class MacVendorLookup:
     @classmethod
     def load_data(cls, url):
         if cls.mac_vendor_data is None:
-            response = requests.get(url)
-            csv_data = StringIO(response.text)
+            cache_file = "oui.csv"
+            if os.path.exists(cache_file):
+                cls.mac_vendor_data = cls._load_from_file(cache_file)
+            else:
+                cls.mac_vendor_data = cls._load_from_url(url)
+                cls._save_to_file(cache_file, cls.mac_vendor_data)
 
-            cls.mac_vendor_data = {}
-            csvreader = csv.reader(csv_data)
-            next(csvreader)
+    @classmethod
+    def _load_from_file(cls, filename):
+        with open(filename, 'r') as file:
+            csvreader = csv.reader(file)
+            next(csvreader)  # Skip header
+            mac_vendor_data = {}
             for row in csvreader:
                 oui = row[1].replace("-", "").upper()[:6]
                 vendor = row[2]
-                cls.mac_vendor_data[oui] = vendor
+                mac_vendor_data[oui] = vendor
+            return mac_vendor_data
+
+    @classmethod
+    def _load_from_url(cls, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            csv_data = StringIO(response.text)
+            csvreader = csv.reader(csv_data)
+            next(csvreader)  # Skip header
+            mac_vendor_data = {}
+            for row in csvreader:
+                oui = row[1].replace("-", "").upper()[:6]
+                vendor = row[2]
+                mac_vendor_data[oui] = vendor
+            return mac_vendor_data
+        else:
+            print(f"Failed to fetch data from {url}. Status code: {response.status_code}")
+            return {}
+
+    @classmethod
+    def _save_to_file(cls, filename, data):
+        with open(filename, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Organizationally Unique Identifier", "Organization Name"])
+            for oui, vendor in data.items():
+                writer.writerow([oui, vendor])
 
     def __init__(self, url):
         self.load_data(url)
--- main.py
+++ main.py
@@ -3,17 +3,14 @@ import argparse
 import sys
 from core.arp_scanner import DeviceDiscoveryDialog, MacVendorLookup
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ARP Sniffer')
     parser.add_argument('--interface', required=True, help='Network interface name')
     args = parser.parse_args()
-    oui_url = "http://standards-oui.ieee.org/oui/oui.csv"
-    MacVendorLookup.load_data(oui_url)
 
     app = QApplication(sys.argv)
 
-    window = DeviceDiscoveryDialog(args.interface, oui_url=oui_url)
+    window = DeviceDiscoveryDialog(args.interface, oui_url="http://standards-oui.ieee.org/oui/oui.csv")
     window.show()
 
     sys.exit(app.exec())
