9962097743b4117b08e790bb988d5394e2f19705
CyberRoute <alessandro.bresciani2016@gmail.com>
2024-03-30 14:54:27 +0100

    refactor

--- core/arp_scanner.py
+++ core/arp_scanner.py
@@ -1,7 +1,5 @@
-import csv
-from io import StringIO
-from PySide6.QtWidgets import QDialog, QListWidgetItem, QLabel, QMainWindow, QVBoxLayout, QWidget, QTextEdit
-import requests
+
+from PySide6.QtWidgets import QDialog, QListWidgetItem, QLabel, QMainWindow, QVBoxLayout, QWidget
 from ui.ui_arpscan import Ui_DeviceDiscovery
 from PySide6.QtGui import QColor, QFont
 from PySide6.QtCore import Slot, Qt, QTimer
@@ -10,66 +8,8 @@ import socket
 import netifaces
 import core.networking as net
 import core.sniffer as sniffer
-import os
+import vendor as vendor
 
-
-class MacVendorLookup:
-    mac_vendor_data = None
-
-    @classmethod
-    def load_data(cls, url):
-        if cls.mac_vendor_data is None:
-            cache_file = "oui.csv"
-            if os.path.exists(cache_file):
-                cls.mac_vendor_data = cls._load_from_file(cache_file)
-            else:
-                cls.mac_vendor_data = cls._load_from_url(url)
-                cls._save_to_file(cache_file, cls.mac_vendor_data)
-
-    @classmethod
-    def _load_from_file(cls, filename):
-        with open(filename, 'r') as file:
-            csvreader = csv.reader(file)
-            next(csvreader)  # Skip header
-            mac_vendor_data = {}
-            for row in csvreader:
-                oui = row[1].replace("-", "").upper()[:6]
-                vendor = row[2]
-                mac_vendor_data[oui] = vendor
-            return mac_vendor_data
-
-    @classmethod
-    def _load_from_url(cls, url):
-        response = requests.get(url)
-        if response.status_code == 200:
-            csv_data = StringIO(response.text)
-            csvreader = csv.reader(csv_data)
-            next(csvreader)  # Skip header
-            mac_vendor_data = {}
-            for row in csvreader:
-                oui = row[1].replace("-", "").upper()[:6]
-                vendor = row[2]
-                mac_vendor_data[oui] = vendor
-            return mac_vendor_data
-        else:
-            print(f"Failed to fetch data from {url}. Status code: {response.status_code}")
-            return {}
-
-    @classmethod
-    def _save_to_file(cls, filename, data):
-        with open(filename, 'w') as file:
-            writer = csv.writer(file)
-            writer.writerow(["Organizationally Unique Identifier", "Organization Name"])
-            for oui, vendor in data.items():
-                writer.writerow([oui, vendor])
-
-    def __init__(self, url):
-        self.load_data(url)
-
-    def lookup_vendor(self, mac_address):
-        cleaned_mac = mac_address.upper().replace(":", "").replace("-", "")
-        oui = cleaned_mac[:6]
-        return self.mac_vendor_data.get(oui, "Vendor not found")
     
 class DeviceDetailsWindow(QMainWindow):
     def __init__(self, ip_address, mac_address, hostname, vendor):
@@ -90,7 +30,7 @@ class DeviceDiscoveryDialog(QDialog):
     def __init__(self, interface, oui_url, parent=None):
         super().__init__(parent)
         self.interface = interface
-        self.mac_vendor_lookup = MacVendorLookup(oui_url)
+        self.mac_vendor_lookup = vendor.MacVendorLookup(oui_url)
 
         self._ui = Ui_DeviceDiscovery()
         self._ui.setupUi(self)
